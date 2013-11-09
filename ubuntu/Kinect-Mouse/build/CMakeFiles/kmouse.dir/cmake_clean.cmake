cf210aa2c3bb8ae297bc5680f32ad60d81b89642
Sergio <sergio@sergio-Satellite-C645.(none)>
2013-11-09 01:49:17 -0600

    Controlador del mouse

---
+++ ubuntu/Kinect-Mouse/build/CMakeFiles/kmouse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/kmouse.dir/kinect_mouse.c.o"
+  "kmouse.pdb"
+  "kmouse"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/kmouse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
